8f4b1bfad4a35b78b34df5589fb42403c75daef0
Clovis <clovajob@yahoo.com>
2020-12-03 22:22:51 -0600

    Change color in fragment_article_item_details.xml

--- app/src/main/java/com/example/journal/utils/ErrorUtils.kt
+++ app/src/main/java/com/example/journal/utils/ErrorUtils.kt
@@ -10,6 +10,7 @@ object ErrorUtils {
     fun handleErrorCode(context: Context, code:Int?): Dialog? {
         return if(code == null) null
             else AlertDialog.Builder(context)
+            .setTitle("Error")
             .setMessage(getMessage(code))
             .setCancelable(true)
             .setOnDismissListener {
@@ -21,14 +22,14 @@ object ErrorUtils {
 
     private fun getMessage(code: Int?) : String {
         return when (code) {
-            400 -> "Bad Request"
-            401 -> "Unauthorized"
-            403 -> "Forbidden"
-            404 -> "Not Found"
-            500 -> "Internal Server Error"
-            502 -> "Bad Gateway"
-            503 -> "Service Unavailable"
-            504 -> "Service Unavailable"
+            400 -> "400 Bad Request"
+            401 -> "401 Unauthorized"
+            403 -> "403 Forbidden"
+            404 -> "404 Not Found"
+            500 -> "500 Internal Server Error"
+            502 -> "502 Bad Gateway"
+            503 -> "503 Service Unavailable"
+            504 -> "504 Service Unavailable"
             else -> {
                 " Unknown Error"
             }
